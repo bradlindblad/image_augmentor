50c4569014aadf67aa19f377402d989de734af07
bradlindblad <bradley.lindblad@gmail.com>
2018-06-06 09:59:25 -0500

    added a command line arg for sample size

--- img_augmentor.py
+++ img_augmentor.py
@@ -1,15 +1,15 @@
 """
 Args:
-    Enter file path of image directory you would like to augment
-    ex: python img_augmentor.py C:\\bobcat_imgs
+    [1] Enter file path of image directory you would like to augment
+    [2] Enter the sample size
+    ex: python img_augmentor.py C:\\bobcat_imgs 100
 
 """
 
 import Augmentor
-import getopt
 import sys
 
-p = Augmentor.Pipeline(sys.argv[1])
+p = Augmentor.Pipeline(sys.argv[1])  # pull in the file path of folder to be augmented
 
 p.rotate90(probability=0.5)
 p.rotate270(probability=0.5)
@@ -20,5 +20,6 @@ p.skew(probability=0.5, magnitude=0.2)
 p.random_contrast(probability=0.7 ,min_factor=1, max_factor=2)
 p.random_distortion(0.4, grid_width=6, grid_height=6, magnitude=5)
 p.rotate(probability=0.4, max_right_rotation=10, max_left_rotation=10)
-p.sample(50)
 
+size =int(sys.argv[2])  # change the argv to an integer
+p.sample(size)
