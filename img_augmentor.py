011bfa288553b73a6e8cce37ed95658955b08e70
bradlindblad <bradley.lindblad@gmail.com>
2018-06-10 19:38:55 -0500

    minor non important adjustments

--- img_augmentor.py
+++ img_augmentor.py
@@ -8,6 +8,7 @@ Args:
 
 import Augmentor
 import sys
+import pyautogui
 
 p = Augmentor.Pipeline(sys.argv[1])  # pull in the file path of folder to be augmented
 
@@ -17,11 +18,12 @@ p.flip_left_right(probability=0.8)
 p.flip_top_bottom(probability=0.3)
 p.shear(probability=0.5, max_shear_left=10, max_shear_right=10)
 p.skew(probability=0.5, magnitude=0.2)
-p.random_contrast(probability=0.7 ,min_factor=1, max_factor=2)
-p.random_distortion(0.4, grid_width=6, grid_height=6, magnitude=5)
+p.random_contrast(probability=0.4, min_factor=1, max_factor=2)
+p.random_distortion(0.8, grid_width=5, grid_height=5, magnitude=4)
 p.rotate(probability=0.4, max_right_rotation=10, max_left_rotation=10)
 
 size = int(sys.argv[2])  # change the argv to an integer
 p.sample(size)
+
 
 
